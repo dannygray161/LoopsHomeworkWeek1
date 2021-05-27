5d7a1eebaf2cca24687c12fcf23fe8e5d048dc47
Daniel Gray <dieseldan14@gmail.com>
2021-05-27 17:44:46 -0500

    Add project files.

---
+++ LoopsHomeworkWeek1/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopsHomeworkWeek1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FIRST PROBLEM
+            // GENERATE ****
+            //          ****
+            //          ****
+
+            //for (int j = 0; j < 4; j++)
+            //{
+            //    Console.WriteLine();
+            //    for (int a = 0; a < 4; a++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //}
+
+            // PROBLEM 2, GENERATE A SORT OF TRIANGLE WITH THE * 
+
+            //for (int i = 0; i <= 4; i++)
+            //{
+            //    for (int a = 0; a <= i; a++)
+            //    {
+            //        Console.Write("*");
+
+            //    }
+
+            //    Console.WriteLine();
+
+            //}
+
+            // PROBLEM 3 reverse problem 2
+
+
+            //int j, k; // declare variables so you can have them for nested for loop 
+            //for (int i = 10; i >= 0; i--) // set a value, then iterate how many times you want loop to run
+            //{
+            //    for (j = i; j >= 0; j--) // same as above, but in order to go down we have to subtract i value from loop iterations
+            //                                 //and set j equal to that for its iterations.
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            // since the first loop is set to <= 5 it will run 5 times, but the second loop will only run 4 due to being subtracted from i
+            // PROBLEM 3 AGAIN TO REITERATE THE CONCEPT BUT WITH NUMBERS. 
+            //int i, j;
+            //for (i = 1; i <=5; i++)
+            //{
+            //    for (j = 1; j <= 5 - i; j++)
+            //}
+
+
+            // PROBLEM 4
+            // print out 1 2 3 4 5 
+            //           1 2 3 4 5
+
+            //int i, j;
+            //for (i = 1; i <= 5; i++)
+            //{
+            //    for (j = 1; j <= 5; j++)
+            //    {
+            //        Console.Write(j);
+            //    }
+            //    Console.WriteLine("");
+            //}
+
+
+            //int i, j;
+            //for (i = 1; i <= 2; i++)
+            //{
+            //    Console.WriteLine("this is loop i " + i);
+
+
+            //    for (j = 1; j <= 3; j++)
+            //    {
+            //        Console.WriteLine("this is loop j " + j);
+            //    }
+            //}
+
+            // PROBLEM 5 11 22 33 
+
+            //for (int i = 1; i <= 4; i++) // i is what i need to print
+            //{
+            //    // Console.WriteLine("this is loop i " + i);
+            //    for (int j = 0; j <= 3; j++)
+            //    {
+            //        Console.Write(i); // this is telling the console to write j times
+
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            // PROBLEM 6 1 22 333
+
+            //for (int i = 0; i <= 4; i++)
+            //{
+            //    for (int j = 0; j <= i; j++)
+            //    {
+            //        Console.Write(i);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            //redo problem 1
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
